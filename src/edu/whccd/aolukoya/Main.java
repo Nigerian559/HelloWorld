6319e66c5eda867d97e0eb6f050cb423ea3cc83d
AlexOlukoya <alex.olukoya@gmail.com>
2020-03-22 20:02:15 -0700

    Hello World Project

---
+++ src/edu/whccd/aolukoya/Main.java
@@ -0,0 +1,11 @@
+package edu.whccd.aolukoya;
+
+/* The purpose of this application is to display Hello World!
+author: Alex Olukoya
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello World!");
+    }
+}
